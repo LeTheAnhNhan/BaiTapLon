1324b26faf943dac929abcabe6941762d400dbe4
LeTheAnhNhan <36005853+LeTheAnhNhan@users.noreply.github.com>
2018-03-20 21:59:32 +0700

    sửa

---
+++ TaiLieu.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Tài Liệu</title>
+	<link rel="stylesheet" type="text/css" href="TaiLieu.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div id="top">
+		<ul class="mn" style="vertical-align: middle;">
+			<li><a href="#">Đăng nhập</a></li>
+			<li><a href="#">Liên Hệ</a></li>
+			<li><a href="#">Giới Thiệu</a></li>
+		</ul>
+	</div>
+
+	<div id="header"><img src="image/1.JPG" alt="" style="margin: 0px auto;"></div>
+
+	<div id="menu">
+		<ul class="mn" style="width: 60%;display: inline-block;vertical-align: top">
+			<li><a href="trangchu.php">Trang Chủ</a></li>
+				<li><a href="tintuc.php">Tin Tức</a></li>
+				<li><a href="thitruong.php">Thị Trường</a></li>
+				<li><a href="tailieu.php">Tài Liệu</a></li>
+				<li><a href="maymoc.php">Công cụ</a></li>	
+		</ul><div class="container" style="width: 40%;display: inline-block;vertical-align: top;line-height: 48px;">
+				<span class="icon"><i class="search" ></i></span>
+				<input type="search" id="search" placeholder="Nhập Từ Khóa Để Tìm Kiếm....">
+				<div id="iconsearch">
+					<i class="fa fa-search" style="padding: 15px 15px;" ></i>
+				</div>
+		</div>
+	</div>
+
+	<div class="slider">
+		<img src="image/2.jpg" width="100%" height="175px">
+	</div>
+	
+	<div class="title">
+		<h2>Tài Liệu Trồng Trọt</h2>
+	</div>
+	<hr  />
+	<div class="content1">
+		<div class="content11">
+			<img class="imgcontent1" src="image/trongtrot.jpg">
+			<div class="bottom">Trồng trọt</div>
+		</div>
+		<div class="content12">
+			<div class="content13">
+				<img class="imgcontent13" src="image/caphe.jpg" alt="">
+				<div class="text">Trồng cà phê</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/trongraucaithao.jpg" alt="">
+				<div class="text">Trồng cải thảo</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/trongraucan.jpg" alt="">
+				<div class="text">Trồng rau cần</div>
+			</div>
+		</div>
+		<div class="content14">
+			<div class="content13">
+				<img class="imgcontent13" src="image/caphe.jpg" alt="">
+				<div class="text">Trồng cà phê</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/trongraucaithao.jpg" alt="">
+				<div class="text">Trồng cải thảo</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/trongraucan.jpg" alt="">
+				<div class="text">Trồng rau cần</div>
+			</div>
+		</div>
+	</div>
+	<hr  />
+	<div class="content2">
+		<div class="content21">
+			<img class="imgcontent2" style="display: inline-block;" src="image/channuoi.jpg">
+		<div class="bottom">Chăn nuôi</div>
+		</div>
+		<div class="content22">
+			<div class="content13">
+				<img class="imgcontent13" src="image/nuoibo.jpg" alt="">
+				<div class="text">Chăn nuôi bò</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/nuoiga.jpg" alt="">
+				<div class="text">Chăn nuôi gà</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/nuoilon.jpg" alt="">
+				<div class="text">chăn nuôi lợn</div>
+			</div>
+		</div>
+		<div class="content23">
+			<div class="content13">
+				<img class="imgcontent13" src="image/nuoibo.jpg" alt="">
+				<div class="text">Chăn nuôi bò</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/nuoiga.jpg" alt="">
+				<div class="text">Chăn nuôi gà</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/nuoilon.jpg" alt="">
+				<div class="text">chăn nuôi lợn</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="footer">
+		<div class="footer1">
+			<h3>
+				<p>Liên Hệ : ..............</p>
+				<p>Email : ................</p>
+			</h3>
+		</div>
+	</div>	
+</body>
+</html>
--- TinTuc.php
+++ TinTuc.php
@@ -4,88 +4,90 @@
 	<meta charset="UTF-8">
 	<title>Tin Tức</title>
 	<link rel="stylesheet" href="TinTuc.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-	<div class="wrap">
-		<div class="top">
-			<ul>
-				<li><a href="#">Liên Hệ</a></li>
-				<li><a href="#">Giới Thiệu</a></li>
-			</ul>
-		</div>
+	<div id="top">
+		<ul class="mn" style="vertical-align: middle;">
+			<li><a href="#">Đăng nhập</a></li>
+			<li><a href="#">Liên Hệ</a></li>
+			<li><a href="#">Giới Thiệu</a></li>
+		</ul>
+	</div>
 
-		<div class="header">
-			<img class="imgheader" src="image/1.JPG" >
-		</div>
-			
-		<div class="menu">
-			<ul>
-				<li><a href="trangchu.php">Trang Chủ</a></li>
+	<div id="header"><img src="image/1.JPG" alt="" style="margin: 0px auto;"></div>
+
+	<div id="menu">
+		<ul class="mn" style="width: 60%;display: inline-block;vertical-align: top">
+			<li><a href="trangchu.php">Trang Chủ</a></li>
 				<li><a href="tintuc.php">Tin Tức</a></li>
 				<li><a href="thitruong.php">Thị Trường</a></li>
 				<li><a href="tailieu.php">Tài Liệu</a></li>
-				<li><a href="maymoc.php">Công cụ</a></li>
-			</ul>
-			<div class="container">
-	      		<input type="search" class="search" placeholder="Search..." />
-	  		</div>
+				<li><a href="maymoc.php">Công cụ</a></li>	
+		</ul><div class="container" style="width: 40%;display: inline-block;vertical-align: top;line-height: 48px;">
+				<span class="icon"><i class="search" ></i></span>
+				<input type="search" id="search" placeholder="Nhập Từ Khóa Để Tìm Kiếm....">
+				<div id="iconsearch">
+					<i class="fa fa-search" style="padding: 15px 15px;" ></i>
+				</div>
 		</div>
+	</div>
 
-		<div class="slider">
-			<img src="image/2.jpg" width="100%" height="175px">
-		</div>
 
-		<div class="content1">
-			<img class="imgcontent1" style="display: inline-block;" src="image/nuoicong.jpg">
-			<div class="bottom">làm giàu nhờ nuôi công</div>
-		</div>
+	<div class="slider">
+		<img src="image/2.jpg" width="100%" height="175px">
+	</div>
 
-		<div class="content2">
-			<div class="content21">
-				<img class="imgcontent21" src="image/nuoide.jpg" alt="">
-				<div class="p">Tiềm năng nghề nuôi dế</div>
-			</div>
-			<div class="content21">
-				<img class="imgcontent21" src="image/trongcaphe.jpg" alt="">
-				<div class="p">Trồng cà phê phát triển mạnh mẽ </div>
-			</div>
-			<div class="content21">
-				<img class="imgcontent21" src="image/trongche.jpg" alt="">
-				<div class="p">Xuất khẩu chè phát triển mạnh</div>
-			</div>
-		</div>
+	<div class="content1">
+		<img class="imgcontent1" style="display: inline-block;" src="image/nuoicong.jpg">
+		<div class="bottom">làm giàu nhờ nuôi công</div>
+	</div>
 
-		<div class="content3">
-			<img class="imgcontent3" style="display: inline-block;" src="image/trongrau.jpg">
-			<div class="bottom1">Trồng rau năng suất cao kiểu Mỹ</div>
+	<div class="content2">
+		<div class="content21">
+			<img class="imgcontent21" src="image/nuoide.jpg" alt="">
+			<div class="p">Tiềm năng nghề nuôi dế</div>
 		</div>
+		<div class="content21">
+			<img class="imgcontent21" src="image/trongcaphe.jpg" alt="">
+			<div class="p">Trồng cà phê phát triển mạnh mẽ </div>
+		</div>
+		<div class="content21">
+			<img class="imgcontent21" src="image/trongche.jpg" alt="">
+			<div class="p">Xuất khẩu chè phát triển mạnh</div>
+		</div>
+	</div>
 
-		<div class="content4">
-			<img class="imgcontent4567" src="image/ca.jpg" alt="">
-			<div class="text"> Xuất khẩu cá ba sa ngày càng mạnh và có tiềm năng vô cùng lớn trong tương lai </div>
-		</div>
-		<div class="content5">
-			<img class="imgcontent4567" src="image/caphe.jpg" alt="">
-			<div class="text">Cà phê trồng ngày càng nhiều ,cần đầu ra phù hợp</div>
-		</div>
-			
-		<div class="content6">
-			<img class="imgcontent4567" src="image/carot.jpg" alt="">
-			<div class="text">Cà rốt Việt Nam được duyệt qua tiêu chuẩn quốc tế, đủ điều kiện để xuất khẩu </div>
-		</div>
+	<div class="content3">
+		<img class="imgcontent3" style="display: inline-block;" src="image/trongrau.jpg">
+		<div class="bottom1">Trồng rau năng suất cao kiểu Mỹ</div>
+	</div>
 
-		<div class="content7">
-			<img class="imgcontent4567" src="image/gao.jpg" alt="">
-			<div class="text">Việt Nam đã vươn lên một bậc trong vị trí các nước xuất khẩu gạo</div>
-		</div>
+	<div class="content4">
+		<img class="imgcontent4567" src="image/ca.jpg" alt="">
+		<div class="text"> Xuất khẩu cá ba sa ngày càng mạnh và có tiềm năng vô cùng lớn trong tương lai </div>
+	</div>
+	<div class="content5">
+		<img class="imgcontent4567" src="image/caphe.jpg" alt="">
+		<div class="text">Cà phê trồng ngày càng nhiều ,cần đầu ra phù hợp</div>
+	</div>
+		
+	<div class="content6">
+		<img class="imgcontent4567" src="image/carot.jpg" alt="">
+		<div class="text">Cà rốt Việt Nam được duyệt qua tiêu chuẩn quốc tế, đủ điều kiện để xuất khẩu </div>
+	</div>
 
-		<div class="footer">
-			<div class="footer1">
-				<h3>
-					<p>Liên Hệ : ..............</p>
-					<p>Email : ................</p>
-				</h2>
-			</div>
+	<div class="content7">
+		<img class="imgcontent4567" src="image/gao.jpg" alt="">
+		<div class="text">Việt Nam đã vươn lên một bậc trong vị trí các nước xuất khẩu gạo</div>
+	</div>
+
+	<div class="footer">
+		<div class="footer1">
+			<h3>
+				<p>Liên Hệ : ..............</p>
+				<p>Email : ................</p>
+			</h2>
 		</div>
 	</div>	
 </body>
