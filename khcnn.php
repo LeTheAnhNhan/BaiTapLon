9683cd24f414de4c1dac94293311bf9ce2001431
tuyenha280697 <36026928+tuyenha280697@users.noreply.github.com>
2018-03-22 21:24:49 +0700

    a

---
+++ khcnn.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Khoa Hoc Công Nghệ</title>
+	<link rel="stylesheet" type="text/css" href="Khcn (2).css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+</head>
+<body>
+	<div id="top">
+		<ul class="a" style="vertical-align: middle;">
+			<li><a href="#">Đăng nhập</a></li>
+			<li><a href="#">Liên Hệ</a></li>
+			<li><a href="#">Giới Thiệu</a></li>
+		</ul>
+	</div>
+
+	<div id="header">
+		<img src="image/1.JPG" alt="" style="margin: 0px auto;">
+	</div>
+	
+	<div id="menu">
+	<ul class="cmenu" style="width: 60%;display: inline-block;vertical-align: top">
+		<li><a href="trangchu.php">Trang Chủ</a></li>
+			<li><a href="tintuc.php">Tin Tức</a></li>
+			<li><a href="thitruong.php">Thị Trường</a></li>
+			<li><a href="tailieu.php">Tài Liệu</a></li>
+			<li><a href="maymoc.php">Công cụ</a></li>	
+	</ul><div class="container" style="width: 40%;display: inline-block;vertical-align: top;line-height: 48px;">
+			<span class="icon"><i class="search" ></i></span>
+			<input type="search" id="search" placeholder="Nhập Từ Khóa Để Tìm Kiếm....">
+			<div id="iconsearch">
+				<i class="fa fa-search" style="padding: 15px 15px;" ></i>
+			</div>
+	</div>
+</div>
+
+	<div class="slider">
+		<img src="image/2.jpg" width="100%" height="175px">
+	</div>
+	
+
+	<div id="content0">
+		<ul>
+			<li><a><b>Khoa Hoc Công Nghệ<b></a></li>
+		</ul>
+	</div>	
+<br>
+	<div class="content1">
+		<div class="content11">
+			<img class="imgcontent1" src=image/U0206.jpg alt="">
+			<div class="bottom">Trong nước</div>
+		</div>
+		<div class="content12">
+			<div class="content13">
+				<img class="imgcontent13" src="image/dpn.jpg" alt="">
+				<div class="text">Điện phân nước trong chăn nuôi</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/hoalan.jpg" alt="">
+				<div class="text">Trồng hoa lan bằng công nghệ cao</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/thanh-long-globalGap.jpg" alt="">
+				<div class="text">Trồng thanh long theo quy trình GlobalGAPs</div>
+			</div>
+		</div>
+	</div>
+
+<hr />
+	<div class="content2">
+		<div class="content21">
+			<img class="imgcontent2" style="display: inline-block;" src="image/athegioi.jpg" alt="">
+		<div class="bottom">Quốc tế</div>
+		</div>
+		<div class="content22">
+			<div class="content13">
+				<img class="imgcontent13" src="image/hoithao.jpg" alt="">
+				<div class="text">Hội thảo quốc tế về Khoa học Nông nghiệp Hữu cơ lần thứ 4 tại Hungary</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/sarat-3.jpg" alt="">
+				<div class="text">Vườn thực nghiệm nông nghiệp thông minh</div>
+			</div>
+			<div class="content13">
+				<img class="imgcontent13" src="image/TQ.jpg" alt="">
+				<div class="text">chăn nuôi lợn</div>
+			</div>
+		</div>
+		
+	</div>
+	<div class="footer">
+		<div class="footer1">
+			<h3>
+				<p>Liên Hệ : ..............</p>
+				<p>Email : ................</p>
+			</h3>
+		</div>
+	</div>	
+
+</body>
+</html>
