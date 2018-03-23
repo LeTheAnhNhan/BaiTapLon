d91b501587da4d9c822a5e7edf4c3ba552120f16
LeTheAnhNhan <36005853+LeTheAnhNhan@users.noreply.github.com>
2018-03-23 22:42:02 +0700

    heku

---
+++ dangnhap.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Đăng Nhập</title>
+	<link rel="stylesheet" href="dangnhap.css" type="text/css">
+</head>
+<body>
+	<form  action="" method="POST" id="frm">
+		<h2>Đăng nhập</h2>
+		<input id="a" type="text" id="user" name="txtuser" placeholder="Tên Đăng Nhập">
+		<input id="a" type="text" id="pass" name="txtpass" placeholder="Mật Khẩu">	
+		<input id="b" type="submit" id="btn" value="Đăng Nhập">			
+	</form>
+	<?php
+		
+	?>
+</body>
+</html>
