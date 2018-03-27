b82ec600bf4ac94b4a1691b7def47218a4ffa52a
tuyenha280697 <36026928+tuyenha280697@users.noreply.github.com>
2018-03-27 20:30:14 +0700

    a

--- connect.php
+++ connect.php
@@ -1,3 +1,5 @@
+<meta charset="utf-8">
 <?php
-	$link = mysqli_connect("localhost","root","","btl-cnweb");
+	$link = mysqli_connect("localhost","root","","btl-cnweb") or die("can't connect database");
+	mysqli_set_charset($link,'utf8');
 ?>
