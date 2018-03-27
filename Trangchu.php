1d98198ae5cb7da4caee2dd063f45977a97d44c4
DoMinhHieu1997 <36026729+DoMinhHieu1997@users.noreply.github.com>
2018-03-27 10:11:11 +0700

    a

--- Trangchu.php
+++ Trangchu.php
@@ -4,6 +4,7 @@
 	<title>Trang Chủ</title>
 	<link rel="stylesheet" type="text/css" href="tc.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="slide.js"></script>
 </head>
 <body>
 	<div id="top">
@@ -31,9 +32,33 @@
 				</div>
 		</div>
 	</div>
-
+	
 	<div id="slider">
-		<img src="image/2.jpg" width="100%" height="175px">
+		<script>
+			var i=0;
+			var images = [];
+			var time = 3000;
+
+			images[0] = 'image/slide1.jpeg';
+			images[1] = 'image/slide2.jpeg';
+			images[2] = 'image/slide3.jpeg';
+			images[3] = 'image/slide4.jpeg';
+			images[4] = 'image/slide5.jpeg';
+			images[5] = 'image/slide6.jpeg';
+
+			function changeimage(){
+				document.slide.src =images[i];
+				if(i<images.length - 1){
+					i++;
+				}else{
+					i=0;
+				}
+				setTimeout("changeimage()",time);
+			}
+
+			window.onload = changeimage;
+		</script>
+		<img name="slide" width="100%" height="100%">
 	</div>
 
 
