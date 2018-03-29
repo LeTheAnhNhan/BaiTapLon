f01eb7a6ef4a1cd6a0b0e0bff62b6126f3435358
LeTheAnhNhan <36005853+LeTheAnhNhan@users.noreply.github.com>
2018-03-29 14:41:20 +0700

    1213231321

--- btl-cnweb.sql
+++ btl-cnweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 25, 2018 lúc 12:14 PM
+-- Thời gian đã tạo: Th3 29, 2018 lúc 09:39 AM
 -- Phiên bản máy phục vụ: 10.1.30-MariaDB
 -- Phiên bản PHP: 7.2.1
 
@@ -26,32 +26,27 @@ DELIMITER $$
 --
 -- Thủ tục
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_suabaidang` (IN `idbaidang` INT(11), IN `tenbaidang` VARCHAR(200), IN `noidung` VARCHAR(3000), IN `anh` VARCHAR(1000), IN `ngaydang` DATE, IN `id` INT(11))  BEGIN
-   UPDATE baidang
-   SET idbaidang=inidbaidang,tenbaidang=intenbaidang,noidung=innoidung,anh=inanh,ngaydang=inngaydang,id=intid;
-END$$
-
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_suathanhvien` (IN `id` INT(11), IN `hoten` VARCHAR(30), IN `tendangnhap` VARCHAR(30), IN `matkhau` VARCHAR(30))  BEGIN
    UPDATE thanhvien
-   SET id=inid,hoten=inhoten,tendangnhap=intendangnhap,matkhau=inmatkhau;
+   SET id=id,hoten=hoten,tendangnhap=tendangnhap,matkhau=matkhau;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_thembaidang` (IN `idbaidang` INT, IN `tenbaidang` VARCHAR(200), IN `noidung` VARCHAR(3000), IN `anh` VARCHAR(1000), IN `ngaydang` DATE, IN `id` INT(11))  BEGIN
-   INSERT INTO baidang(idbaidang,tenbaidang,noidung,anh,ngaydang,id) VALUES (inidbaidang,intenbaidang,innoidung,inanh,inngaydang,inid);
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_thembaidang` (IN `idbaidang` INT(11), IN `tenbaidang` VARCHAR(200), IN `noidung` VARCHAR(10000), IN `anh` VARCHAR(1000), IN `ngaydang` VARCHAR(30), IN `id` INT(11))  BEGIN
+   INSERT INTO baidang(idbaidang,tenbaidang,noidung,anh,ngaydang,id) VALUES (idbaidang,tenbaidang,noidung,anh,ngaydang,id);
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_themthanhvien` (IN `id` INT(11), IN `hoten` VARCHAR(30), IN `tendangnhap` VARCHAR(30), IN `matkhau` VARCHAR(30))  BEGIN
-   INSERT INTO thanhvien (id,hoten,tendangnhap,matkhau) VALUES (inid,inhoten,intendangnhap,inmatkhau);
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_themthanhvien` (IN `id` INT(11) UNSIGNED, IN `hoten` VARCHAR(30), IN `tendangnhap` VARCHAR(30), IN `matkhau` VARCHAR(30))  BEGIN
+   INSERT INTO thanhvien (id,hoten,tendangnhap,matkhau) VALUES (id,hoten,tendangnhap,matkhau);
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_xoabaidang` (IN `idbaidang` INT(11))  BEGIN
    DELETE FROM baidang
-   WHERE idbaidang = inidbaidang;
+   WHERE idbaidang = idbaidang;
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_xoathanhvien` (IN `id` INT(11))  BEGIN
    DELETE FROM thanhvien
-   WHERE id = inid;
+   WHERE id =id;
 END$$
 
 DELIMITER ;
@@ -59,15 +54,42 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `baidang`
+-- Cấu trúc bảng cho bảng `congcu`
 --
 
-CREATE TABLE `baidang` (
+CREATE TABLE `congcu` (
   `idbaidang` int(11) NOT NULL,
   `tenbaidang` varchar(200) COLLATE utf32_unicode_ci DEFAULT NULL,
-  `noidung` varchar(3000) COLLATE utf32_unicode_ci DEFAULT NULL,
+  `noidung` mediumtext COLLATE utf32_unicode_ci,
   `anh` varchar(1000) COLLATE utf32_unicode_ci DEFAULT NULL,
-  `ngaydang` date DEFAULT NULL,
+  `id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khoahoc`
+--
+
+CREATE TABLE `khoahoc` (
+  `idbaidang` int(11) NOT NULL,
+  `tenbaidang` varchar(200) COLLATE utf32_unicode_ci DEFAULT NULL,
+  `noidung` mediumtext COLLATE utf32_unicode_ci,
+  `anh` varchar(1000) COLLATE utf32_unicode_ci DEFAULT NULL,
+  `id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nongdan`
+--
+
+CREATE TABLE `nongdan` (
+  `idbaidang` int(11) NOT NULL,
+  `tenbaidang` varchar(200) COLLATE utf32_unicode_ci DEFAULT NULL,
+  `noidung` mediumtext COLLATE utf32_unicode_ci,
+  `anh` varchar(1000) COLLATE utf32_unicode_ci DEFAULT NULL,
   `id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
 
@@ -89,16 +111,58 @@ CREATE TABLE `thanhvien` (
 --
 
 INSERT INTO `thanhvien` (`id`, `hoten`, `tendangnhap`, `matkhau`) VALUES
-(1, 'le the anh nhan', 'nhan', '1');
+(1, 'nhan', 'nhan', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `thitruong`
+--
+
+CREATE TABLE `thitruong` (
+  `idbaidang` int(11) NOT NULL,
+  `tenbaidang` varchar(200) COLLATE utf32_unicode_ci DEFAULT NULL,
+  `noidung` mediumtext COLLATE utf32_unicode_ci,
+  `anh` varchar(1000) COLLATE utf32_unicode_ci DEFAULT NULL,
+  `id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tintuc`
+--
+
+CREATE TABLE `tintuc` (
+  `idbaidang` int(11) NOT NULL,
+  `tenbaidang` varchar(200) COLLATE utf32_unicode_ci DEFAULT NULL,
+  `noidung` mediumtext COLLATE utf32_unicode_ci,
+  `anh` varchar(1000) COLLATE utf32_unicode_ci DEFAULT NULL,
+  `id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
 
 --
 -- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Chỉ mục cho bảng `baidang`
+-- Chỉ mục cho bảng `congcu`
 --
-ALTER TABLE `baidang`
+ALTER TABLE `congcu`
+  ADD PRIMARY KEY (`idbaidang`),
+  ADD KEY `id` (`id`);
+
+--
+-- Chỉ mục cho bảng `khoahoc`
+--
+ALTER TABLE `khoahoc`
+  ADD PRIMARY KEY (`idbaidang`),
+  ADD KEY `id` (`id`);
+
+--
+-- Chỉ mục cho bảng `nongdan`
+--
+ALTER TABLE `nongdan`
   ADD PRIMARY KEY (`idbaidang`),
   ADD KEY `id` (`id`);
 
@@ -109,14 +173,92 @@ ALTER TABLE `thanhvien`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Chỉ mục cho bảng `thitruong`
+--
+ALTER TABLE `thitruong`
+  ADD PRIMARY KEY (`idbaidang`),
+  ADD KEY `KE` (`id`);
+
+--
+-- Chỉ mục cho bảng `tintuc`
+--
+ALTER TABLE `tintuc`
+  ADD PRIMARY KEY (`idbaidang`),
+  ADD KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `congcu`
+--
+ALTER TABLE `congcu`
+  MODIFY `idbaidang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `khoahoc`
+--
+ALTER TABLE `khoahoc`
+  MODIFY `idbaidang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `nongdan`
+--
+ALTER TABLE `nongdan`
+  MODIFY `idbaidang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `thanhvien`
+--
+ALTER TABLE `thanhvien`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `thitruong`
+--
+ALTER TABLE `thitruong`
+  MODIFY `idbaidang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `tintuc`
+--
+ALTER TABLE `tintuc`
+  MODIFY `idbaidang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- Các ràng buộc cho bảng `baidang`
+-- Các ràng buộc cho bảng `congcu`
 --
-ALTER TABLE `baidang`
-  ADD CONSTRAINT `baidang_ibfk_1` FOREIGN KEY (`id`) REFERENCES `thanhvien` (`id`);
+ALTER TABLE `congcu`
+  ADD CONSTRAINT `congcu_ibfk_1` FOREIGN KEY (`id`) REFERENCES `thanhvien` (`id`);
+
+--
+-- Các ràng buộc cho bảng `khoahoc`
+--
+ALTER TABLE `khoahoc`
+  ADD CONSTRAINT `khoahoc_ibfk_1` FOREIGN KEY (`id`) REFERENCES `thanhvien` (`id`);
+
+--
+-- Các ràng buộc cho bảng `nongdan`
+--
+ALTER TABLE `nongdan`
+  ADD CONSTRAINT `nongdan_ibfk_1` FOREIGN KEY (`id`) REFERENCES `thanhvien` (`id`);
+
+--
+-- Các ràng buộc cho bảng `thitruong`
+--
+ALTER TABLE `thitruong`
+  ADD CONSTRAINT `KE` FOREIGN KEY (`id`) REFERENCES `thanhvien` (`id`);
+
+--
+-- Các ràng buộc cho bảng `tintuc`
+--
+ALTER TABLE `tintuc`
+  ADD CONSTRAINT `tintuc_ibfk_1` FOREIGN KEY (`id`) REFERENCES `thanhvien` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
