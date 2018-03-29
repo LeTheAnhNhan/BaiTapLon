201420566af768a53fe429d5e2b58da9f18727b6
LeTheAnhNhan <36005853+LeTheAnhNhan@users.noreply.github.com>
2018-03-29 17:25:30 +0700

    đổ dữu liệu ra trang chủ bằng php

--- btl-cnweb.sql
+++ btl-cnweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 29, 2018 lúc 09:39 AM
+-- Thời gian đã tạo: Th3 29, 2018 lúc 10:59 AM
 -- Phiên bản máy phục vụ: 10.1.30-MariaDB
 -- Phiên bản PHP: 7.2.1
 
@@ -21,35 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Cơ sở dữ liệu: `btl-cnweb`
 --
-
-DELIMITER $$
---
--- Thủ tục
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_suathanhvien` (IN `id` INT(11), IN `hoten` VARCHAR(30), IN `tendangnhap` VARCHAR(30), IN `matkhau` VARCHAR(30))  BEGIN
-   UPDATE thanhvien
-   SET id=id,hoten=hoten,tendangnhap=tendangnhap,matkhau=matkhau;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_thembaidang` (IN `idbaidang` INT(11), IN `tenbaidang` VARCHAR(200), IN `noidung` VARCHAR(10000), IN `anh` VARCHAR(1000), IN `ngaydang` VARCHAR(30), IN `id` INT(11))  BEGIN
-   INSERT INTO baidang(idbaidang,tenbaidang,noidung,anh,ngaydang,id) VALUES (idbaidang,tenbaidang,noidung,anh,ngaydang,id);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_themthanhvien` (IN `id` INT(11) UNSIGNED, IN `hoten` VARCHAR(30), IN `tendangnhap` VARCHAR(30), IN `matkhau` VARCHAR(30))  BEGIN
-   INSERT INTO thanhvien (id,hoten,tendangnhap,matkhau) VALUES (id,hoten,tendangnhap,matkhau);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_xoabaidang` (IN `idbaidang` INT(11))  BEGIN
-   DELETE FROM baidang
-   WHERE idbaidang = idbaidang;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_xoathanhvien` (IN `id` INT(11))  BEGIN
-   DELETE FROM thanhvien
-   WHERE id =id;
-END$$
-
-DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -142,6 +113,15 @@ CREATE TABLE `tintuc` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
 
 --
+-- Đang đổ dữ liệu cho bảng `tintuc`
+--
+
+INSERT INTO `tintuc` (`idbaidang`, `tenbaidang`, `noidung`, `anh`, `id`) VALUES
+(1, 'Cuộc chiến chống \'thịt giả\' đã bắt đầu: Thịt là gì?', 'Thuở trước, sự phân biệt thực đơn giản. Động vật là thịt, và thực vật không phải thịt. Nhưng hiện nay, do nuôi trồng, việc phân biệt khó hơn rất nhiều, hoặc có người gọi thẳng ra là thịt “giả”.  Những công ty như Impossible Foods và Beyond Meat đang sử dụng các phòng lab khoa học và nông trại thay cho thịt động vật, để tạo ra các sản phẩm cạnh tranh với những món nướng truyền thống như burger và hot dog. Hiệp hội những người chăn nuôi Mỹ đang vẽ ra một đường rạch ròi và bắt đầu nã “loạt súng” đầu tiên trong một cuộc chiến lâu dài vào thực phẩm gốc thực vật. Khoảng đầu tháng này, hiệp hội gởi một kiến nghị dài 15 trang cho bộ Nông nghiệp Mỹ kêu gọi có một định nghĩa chính thức đối với từ “thịt bò”, và nói rộng hơn là “thịt”.  “Trong khi vào lúc này các nguồn protein thay thế không phải là đe dọa trực tiếp đến ngành chăn nuôi bò, chúng tôi nhận thấy việc dán nhãn các sản phẩm không phù hợp chẳng khác nào lừa dối”, Lia Biondo, giám đốc chính sách và phát triển của hiệp hội Những người chăn nuôi, nói. “Mục đích của chúng tôi là ngăn chặn ngay từ đầu trước khi vấn đề trở nên trầm trọng”.', 'https://baomoi-photo-1-td.zadn.vn/w700_r1m/18/03/03/105/25126764/1_43804.jpg', 1),
+(2, 'Sản xuất, chế biến thịt, sữa… có lo ngại CPTPP?', 'Hiệp định Đối tác toàn diện và tiến bộ xuyên Thái Bình Dương (CPTPP) với 11 nước thành viên sẽ chính thức được ký kết tại Chi lê vào ngày 8/3 tới đây.  CPTPP sẽ mang đến tự do hóa thương mại, hàng hóa được lưu thông tự do trong khối 11 nước, giúp Việt Nam có nhiều cơ hội xuất khẩu. Nhưng đi cùng với đó, thị trường trong nước cũng sẽ phải đối mặt với sự cạnh tranh quyết liệt, đặc biệt là ngành nông nghiệp.', 'http://media.baodautu.vn/thumb_x470x250/Images/manhcuong/2018/03/02/san-xuat-che-bien-thit-sua-co-lo-ngai-cptpp1519954544.jpg', 1),
+(3, 'Hạn chế đầu tư nhà máy thức ăn chăn nuôi', 'Rõ ràng, cơ cấu phân bổ nhà máy thức ăn chăn nuôi ở nước ta rất không đều. Vì vậy, cần khuyến cáo các tỉnh hạn chế mở mới, đặc biệt ở các vùng có mật độ nhà máy cao\",ông Nguyễn Xuân Dương, Cục phó cục chăn nuôi cho biết.  Trong quy hoạch của Bộ Nông nghiệp và Phát triển nông thôn, đến năm 2020 công suất các nhà máy thức ăn chăn nuôi công nghiệp đạt khoảng 25 triệu tấn, sản lượng thực tế khoảng 17 triệu tấn nhưng đến năm 2017 nước ta đã đạt công suất 31 triệu tấn, sản lượng thực tế khoảng 21 triệu tấn.  Như vậy, ngành công nghiệp thức ăn chăn nuôi của nước ta đã vượt quy hoạch nên chắc chắn sẽ dẫn đến tình trạng cung vượt cầu rất lớn, đó là chưa kể đến những tác động đối với môi trường', 'https://baomoi-photo-3-td.zadn.vn/w700_r1m/18/03/02/256/25121924/2_100488.jpg', 1);
+
+--
 -- Chỉ mục cho các bảng đã đổ
 --
 
@@ -224,7 +204,7 @@ ALTER TABLE `thitruong`
 -- AUTO_INCREMENT cho bảng `tintuc`
 --
 ALTER TABLE `tintuc`
-  MODIFY `idbaidang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idbaidang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
